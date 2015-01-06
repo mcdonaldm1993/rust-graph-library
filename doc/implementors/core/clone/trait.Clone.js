165701d4984271dbb1e5d9d6aedde292b0e40fa3
Michael McDonald <mcdonaldm1993@gmail.com>
2015-01-06 22:31:50 +0000

    - Added docs

---
+++ doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['graph'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;I: <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='graph/struct.GraphPath.html' title='graph::GraphPath'>GraphPath</a>&lt;I&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;I: <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>, D: <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='graph/struct.UndirectedAdjListGraph.html' title='graph::UndirectedAdjListGraph'>UndirectedAdjListGraph</a>&lt;I, D&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['graph'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl&lt;I: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='graph/struct.GraphPath.html' title='graph::GraphPath'>GraphPath</a>&lt;I&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
