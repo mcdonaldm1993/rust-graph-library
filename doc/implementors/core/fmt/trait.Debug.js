dcfb89fcdc2482e1dc9661b8ec781c158318c8ce
Michael McDonald <mcdonaldm1993@gmail.com>
2015-02-10 19:04:40 +0000

    - Updated docs to commit: eee59afe2a8471ebd61f1b4adc8dee63f6da4439

---
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['graph'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;N: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='graph/struct.GraphPath.html' title='graph::GraphPath'>GraphPath</a>&lt;N&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
